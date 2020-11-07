3573d2f74215ddefb5806ba5e7a81204f22c35da
spanri <kozlova9v@mail.ru>
2020-11-07 17:14:25 +0300

    do ffmpeg work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "regenerator-runtime/runtime.js";
 import "./App.scss";
-
-import { createFFmpeg } from "@ffmpeg/ffmpeg";
 import Button from "ui-components/Button";
 import File from "components/File";
-
 import "./assets/styles/index.scss";
+
+import { createFFmpeg } from "@ffmpeg/ffmpeg";
+import { transliterate } from "./file.helper";
 
 class App extends React.Component {
   constructor(props) {
@@ -16,7 +16,10 @@ class App extends React.Component {
       file: null,
       fileImage: null,
       fileVideo: null,
+      // Is button "Convert" pressed?
       isConverting: false,
+      // What to write in the loading plate
+      convertingStatus: "",
     };
   }
 
@@ -63,42 +66,46 @@ class App extends React.Component {
   }
 
   async doTranscode() {
+    this.setState({ convertingStatus: "doing" });
     this.setState({ isConverting: true });
 
     try {
-      const ffmpeg = createFFmpeg();
+      const transliterateFileName = (fileName) => {
+        const fileNameArray = fileName.split(" ");
+        return fileNameArray.map((word) => transliterate(word)).join("_");
+      };
+
+      const file = this.state.file;
+      var re = /(?:\.([^.]+))?$/;
+      var fileName = transliterateFileName(file.name.replace(re, ""));
+      var fileExtension = re.exec(file.name)[1];
+      const ffmpeg = createFFmpeg({ log: true });
+
+      this.setState({ convertingStatus: "preparing" });
+
       await ffmpeg.load();
-      await ffmpeg.write(this.state.file.name, this.state.file);
+      await ffmpeg.write(`input.${fileExtension}`, file);
+
+      this.setState({ convertingStatus: "converting" });
+
       await ffmpeg.run(
-        "-loop",
-        "1",
-        "-r",
-        "30",
-        "-i",
-        this.state.file.name,
-        "-t",
-        "5",
-        "-vf",
-        "'scale=1440:720,format=yuv420p'",
-        "-codec:v",
-        "libx264",
-        "output.mp4"
+        `-loop 1 -r 30 -i "input.${fileExtension}" -t 5 -vf "scale=1440:720,format=yuv420p" -codec:v libx264 ${fileName}.mp4`
       );
-      // await ffmpeg.transcode("test.jpg", "test.mp4", );
-      console.log("pre-data", ffmpeg);
-      const data = ffmpeg.read("output.mp4");
-      console.log("data", data);
+
+      this.setState({ convertingStatus: "loading" });
+
+      const data = ffmpeg.read(`${fileName}.mp4`);
       this.setState({
         fileVideo: URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" })),
       });
 
       this.setState({ isVideoConverted: true });
     } catch (error) {
-      console.log(error);
       alert("Error: " + error.message);
     }
 
     this.setState({ isConverting: false });
+    this.setState({ convertingStatus: "doing" });
   }
 
   render() {
@@ -113,34 +120,35 @@ class App extends React.Component {
             <div className="app__header-description">Image to video conversion</div>
           </header>
 
-          <div className="app__buttons">
-            <div className={`app__button-wrapper ${this.isFileSelected ? "selected" : ""}`}>
-              <Button theme={this.isFileSelected ? "gray" : "primary-animated"}>
-                <div className="app__button">
-                  <input
-                    className="app__button-input"
-                    type="file"
-                    accept="image/*"
-                    onChange={(event) => this.onChangeFile(event)}
-                  />
-                  <span>{this.state.file ? "Change image" : "Select image"}</span>
-                </div>
-              </Button>
-            </div>
-
-            {this.isFileSelected ? (
+          {this.state.isConverting ? (
+            <div className="app__doing loading">{this.state.convertingStatus}</div>
+          ) : (
+            <div className={"app__buttons"}>
               <div className={`app__button-wrapper ${this.isFileSelected ? "selected" : ""}`}>
-                {this.state.isConverting}
-                <Button disabled={this.state.isConverting} onClick={() => this.doTranscode()}>
+                <Button theme={this.isFileSelected ? "gray" : "primary-animated"}>
                   <div className="app__button">
-                    <span>Convert</span>
+                    <input
+                      className="app__button-input"
+                      type="file"
+                      accept="image/*"
+                      onChange={(event) => this.onChangeFile(event)}
+                    />
+                    <span>{this.state.file ? "Change image" : "Select image"}</span>
                   </div>
                 </Button>
-
-                {this.state.isConverting ? <div className="app__doing">doing...</div> : null}
               </div>
-            ) : null}
-          </div>
+
+              {this.isFileSelected ? (
+                <div className={`app__button-wrapper ${this.isFileSelected ? "selected" : ""}`}>
+                  <Button disabled={this.state.isConverting} onClick={() => this.doTranscode()}>
+                    <div className="app__button">
+                      <span>Convert</span>
+                    </div>
+                  </Button>
+                </div>
+              ) : null}
+            </div>
+          )}
 
           {this.isFileSelected ? (
             <File
